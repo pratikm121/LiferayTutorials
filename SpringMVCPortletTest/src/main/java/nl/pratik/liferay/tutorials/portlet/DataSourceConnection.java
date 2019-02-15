d0044cfa6f4e98cb7d2d47b171230b287c17c456
Pratik Mehta <38359485+pratikm121@users.noreply.github.com>
2019-02-15 20:19:51 +0100

    Added DataSourceConnection class for Datasrouce connection.

---
+++ SpringMVCPortletTest/src/main/java/nl/pratik/liferay/tutorials/portlet/DataSourceConnection.java
@@ -0,0 +1,41 @@
+package nl.pratik.liferay.tutorials.portlet;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DataSourceConnection {
+	
+	
+	public String getData() throws SQLException, NamingException {
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		String JNDI = "jdbc/liferay";
+		Context context = new InitialContext();
+		Context envContext  = (Context)context.lookup("java:/comp/env");
+		DataSource ds = (DataSource)envContext.lookup(JNDI);		
+		conn = ds.getConnection();
+		
+	    if(conn !=null) {
+	    	System.out.println("Got the connection");
+	    }
+	    String sql = "Select empname from public.employees where emplid = 2";
+	    ps = conn.prepareStatement(sql);
+	    rs = ps.executeQuery();
+	    String output = null;
+	    while(rs.next()){
+	    	output = rs.getString("empname");
+	    	System.out.println("output= "+output);
+	    }
+	    rs.close();
+	    
+	    return output;
+	}
+}
